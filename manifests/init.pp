bbbfcf402c80aebe240cf4fede62e2235be6f0c7
Christoph Görn <goern@redhat.com>
2015-08-19 18:01:23 +0200

    also need to remove nm-tui

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,8 +36,7 @@
 # Copyright 2015 Your name here, unless otherwise noted.
 #
 class ose3prerequisites {
- package { "NetworkManager":
-     ensure => "absent",
-  }
+  package { "NetworkManager-tui": ensure => "absent" }
+  package { "NetworkManager": ensure => "absent" }
 
 }
